ce8816f08a1f20476a3a2faa07de52c20e401251
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-25 10:35:08 +0300

    Upload default project to GitHub

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # book-reservation-app
+
+A CRUD project with Spring Boot and React.
---
+++ backend/src/test/java/lt/techin/bookreservationapp/BookReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.bookreservationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
