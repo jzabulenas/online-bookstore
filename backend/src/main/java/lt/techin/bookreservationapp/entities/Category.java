7f7b69eab022817c0e3a263dc69268bf360ef39a
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-26 13:06:47 +0300

    Add generation type and remove setId method

--- backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
+++ backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
@@ -1,24 +1,18 @@
 package lt.techin.bookreservationapp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Categories")
 public class Category {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
