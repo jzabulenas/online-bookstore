b991c3c097ac4cae7215e63130826ec02d62471b
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-27 15:29:28 +0300

    Modify Add component to send category name to API endpoint

--- frontend/src/components/Add.js
+++ frontend/src/components/Add.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 export default function Add() {
+  const [categoryField, setCategoryField] = useState({
+    name: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCategoryField({
+      ...categoryField,
+      name: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataToPost = {
+      name: categoryField.name,
+    };
+
+    fetch("http://localhost:8080/categories", {
+      method: "POST",
+      body: JSON.stringify(dataToPost),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Category title:</label>
       <input
         className="form-control"
         type="text"
         name="category"
+        value={categoryField.name}
+        onChange={handleChange}
       />
       <button
         className="btn btn-primary"
