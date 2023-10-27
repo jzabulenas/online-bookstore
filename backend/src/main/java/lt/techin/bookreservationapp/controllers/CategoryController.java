76ea6b98142d3a22ba24b621f0722bcc09c752ed
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-27 10:09:30 +0300

    Add custom errror message when incorrect value is sent
    Additionally, update the HTTP response status code to return 201 when a record is successfully created and 400 when a request contains incorrect values.

--- backend/src/main/java/lt/techin/bookreservationapp/controllers/CategoryController.java
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/CategoryController.java
@@ -1,8 +1,13 @@
 package lt.techin.bookreservationapp.controllers;
 
+import jakarta.validation.Valid;
 import lt.techin.bookreservationapp.entities.Category;
 import lt.techin.bookreservationapp.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,8 +31,31 @@ public class CategoryController {
         return categoryRepository.findById(id).get();
     }
 
+//    @PostMapping("/categories")
+//    public void addCategory(@RequestBody Category category) {
+//        categoryRepository.save(category);
+//    }
+
+
     @PostMapping("/categories")
-    public void addCategory(@RequestBody Category category) {
+    public ResponseEntity<String> addPerson(@Valid @RequestBody Category category, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorMessageBuilder = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            for (int i = 0; i < fieldErrors.size(); i++) {
+                FieldError fieldError = fieldErrors.get(i);
+                errorMessageBuilder.append(fieldError.getDefaultMessage());
+                if (i < fieldErrors.size() - 1) {
+                    errorMessageBuilder.append(" | ");
+                }
+            }
+
+            String errorMessage = errorMessageBuilder.toString();
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
         categoryRepository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
 }
--- backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
+++ backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
@@ -12,9 +12,11 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "^(?!.*(.)\\1)[A-Z][a-z]{4,49}$")
+    @NotNull(message = "The field must not be null")
+    @NotEmpty(message = "The field must not be empty")
+    @Pattern(regexp = "^(?!.*(.)\\1)[A-Z][a-z]{4,49}$", message = "Category name must start with an uppercase " +
+            "letter, followed by lowercase letters, without numbers, consecutive repeated characters, " +
+            "and a length between 5 and 50 characters")
     @Column(unique = true)
     private String name;
 
