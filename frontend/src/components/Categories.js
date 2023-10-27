9132985b8c94074bb7d0d42612dc52287e81cc6f
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-27 12:50:06 +0300

    Implement fetching from API and logging out data to console

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    let active = true;
+
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/categories");
+      const data = await response.json();
+
+      if (active) {
+        setCategories(data);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      active = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Kategorijos"); // Log the updated categories content
+    console.log(categories); // Log the updated categories content
+  }, [categories]);
+
   return (
     <div className="container">
       <label htmlFor="category">Category:</label>
