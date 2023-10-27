74d9b8bcb2fdbe51fb800d4321df4ab5fc6e5c8c
Egidijus <egidijus.macernis@stud.techin.lt>
2023-10-27 10:56:40 +0300

    update category component to have a dropdown list

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,3 +1,21 @@
 export default function Categories() {
-  return <div>Welcome to Categories!</div>;
+  return (
+    <div className="container">
+      <label htmlFor="category">Category:</label>
+      <select
+        className="form-select"
+        name="category"
+      >
+        <option
+          defaultValue="Select a category"
+          disabled
+        >
+          Select a category
+        </option>
+        <option name="science-fiction">Science </option>
+        <option name="fantasy">Fantasy</option>
+        <option name="self-help">Self Help</option>
+      </select>
+    </div>
+  );
 }
