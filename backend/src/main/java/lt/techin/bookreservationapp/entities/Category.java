3278137ff7ee6e114586fe44c4e85422f154051e
Egidijus <egidijus.macernis@stud.techin.lt>
2023-11-03 16:10:46 +0200

    Update regular expression to accept symbols
    Also accepts no more then 2 repeated characters

--- backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
+++ backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
@@ -14,9 +14,15 @@ public class Category {
 
     @NotNull(message = "The field must not be null")
     @NotEmpty(message = "The field must not be empty")
-    @Pattern(regexp = "^(?!.*(.)\\1)[A-Z][a-z ]{4,49}$", message = "Category name must start with an uppercase " +
+//    @Pattern(regexp = "^(?!.*(.)\\1)[A-Z][a-z ]{4,49}$", message = "Category name must start with an uppercase " +
+//            "letter, followed by lowercase letters, without numbers, consecutive repeated characters, " +
+//            "and a length between 5 and 50 characters")
+
+
+    @Pattern(regexp = "^(?!.*([A-Za-z])\\1\\1)[A-Z][a-z\\s\\W]{4,49}$", message = "Category name must start with an uppercase " +
             "letter, followed by lowercase letters, without numbers, consecutive repeated characters, " +
             "and a length between 5 and 50 characters")
+
     @Column(unique = true)
     private String name;
 
