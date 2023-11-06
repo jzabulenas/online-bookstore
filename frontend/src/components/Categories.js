361e02c8adb6bb684bfc7639ee01935ab76e5813
Egidijus <egidijus.macernis@stud.techin.lt>
2023-11-06 12:55:16 +0200

    Add Cancel button to cancel category adding

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -57,7 +57,7 @@ export default function Categories() {
       >
         Add new category
       </button>
-      {addClicked && <Add />}
+      {addClicked && <Add setAddClicked={setAddClicked} />}
     </div>
   );
 }
