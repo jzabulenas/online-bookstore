ec3208271f42cb60f313582079f43012bc0718be
Egidijus <egidijus.macernis@stud.techin.lt>
2023-11-10 16:56:25 +0200

    Add BookRepository interface

---
+++ backend/src/main/java/lt/techin/bookreservationapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package lt.techin.bookreservationapp.repositories;
+
+import lt.techin.bookreservationapp.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Integer> {
+}
