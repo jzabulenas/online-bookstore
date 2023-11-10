925d3060f80eb598858643f979498240f5adad82
Egidijus <egidijus.macernis@stud.techin.lt>
2023-11-10 17:02:52 +0200

    Implement getBooks method to get all books from DB

--- backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
@@ -1,9 +1,13 @@
 package lt.techin.bookreservationapp.controllers;
 
+import lt.techin.bookreservationapp.entities.Book;
 import lt.techin.bookreservationapp.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -13,5 +17,10 @@ public class BookController {
     @Autowired
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
     }
 }
