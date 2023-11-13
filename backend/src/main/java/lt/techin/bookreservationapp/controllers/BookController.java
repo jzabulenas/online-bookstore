629bf1395365cf770822bb4ad4f0e7f60ee69764
Egidijus <egidijus.macernis@stud.techin.lt>
2023-11-13 09:40:33 +0200

    Implement getBook method to get a book by id
    Return status code 200 if success, otherwise - status code 404

--- backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
@@ -1,13 +1,17 @@
 package lt.techin.bookreservationapp.controllers;
 
 import lt.techin.bookreservationapp.entities.Book;
+import lt.techin.bookreservationapp.entities.Category;
 import lt.techin.bookreservationapp.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -22,5 +26,17 @@ public class BookController {
     @GetMapping("/books")
     public List<Book> getBooks() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable int id) {
+
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            return ResponseEntity.ok(book.get());
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 }
