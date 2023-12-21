7b897e780c9180472c8da0140c5e7b2b32ae85e6
Julius <jzabulenas@protonmail.com>
2023-12-21 13:00:05 +0200

    Use BCryptPasswordEncoder Bean

--- backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,8 +30,8 @@ public class SecurityConfig {
 	}
 	
 
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
