e95b9dcb3150a961b6c49f3193934ee738d5461a
Julius <jzabulenas@protonmail.com>
2023-12-21 13:59:35 +0200

    Implement AuthController

---
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/AuthController.java
@@ -0,0 +1,43 @@
+package lt.techin.bookreservationapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lt.techin.bookreservationapp.entities.User;
+import lt.techin.bookreservationapp.repositories.UserRepository;
+
+@RestController
+public class AuthController {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public AuthController(UserRepository userRepository,
+			PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@PostMapping("/signup")
+	public void signup(@RequestBody User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setRole("USER");
+
+		userRepository.save(user);
+	}
+
+	@PostMapping("/login")
+	public User login(@RequestBody User user) {
+		User userDb = userRepository
+				.findUserByUsername(user.getUsername())
+				.filter(uDb -> passwordEncoder.matches(user.getPassword(),
+						uDb.getPassword()))
+				.get();
+
+		return userDb;
+	}
+}
