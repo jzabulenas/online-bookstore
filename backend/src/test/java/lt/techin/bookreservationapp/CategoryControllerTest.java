ca2dd01651083cf913c1308ada5709300791911c
Julius <jzabulenas@protonmail.com>
2024-01-28 21:44:30 +0200

    Add test to see if a category gets added successfully

--- backend/src/test/java/lt/techin/bookreservationapp/CategoryControllerTest.java
+++ backend/src/test/java/lt/techin/bookreservationapp/CategoryControllerTest.java
@@ -1,7 +1,10 @@
 package lt.techin.bookreservationapp;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,18 +12,22 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lt.techin.bookreservationapp.controllers.CategoryController;
 import lt.techin.bookreservationapp.entities.Category;
 import lt.techin.bookreservationapp.repositories.CategoryRepository;
 import lt.techin.bookreservationapp.services.CategoryService;
 
-@WebMvcTest(controllers = CategoryController.class)
-@WithMockUser(value = "test-user")
+@WebMvcTest(controllers = CategoryController.class,
+		excludeAutoConfiguration = { SecurityAutoConfiguration.class })
+//@WithMockUser
 public class CategoryControllerTest {
 
 	@Autowired
@@ -51,5 +58,32 @@ public class CategoryControllerTest {
 
 		mockMvc.perform(get("/categories"))
 				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void addCategory_whenSaveCategory_thenReturnIt() throws Exception {
+//		given
+		Category category = new Category("Health, Fitness & Dieting");
+//		BindingResult bindingResult = mock(BindingResult.class);
+//		given(ValidationService.processFieldErrors(bindingResult))
+//				.willReturn(false);
+//		given(bindingResult.hasErrors()).willReturn(false);
+		given(categoryService.existsByName(category.getName()))
+				.willReturn(false);
+		given(categoryService.save(any(Category.class)))
+				.willReturn(category);
+
+//		when
+		mockMvc.perform(post("/categories")
+//		then
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(category))
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(
+						jsonPath("name").value("Health, Fitness & Dieting"));
+
+		// Verify that the save method was called
+		verify(categoryService).save(any(Category.class));
 	}
 }
