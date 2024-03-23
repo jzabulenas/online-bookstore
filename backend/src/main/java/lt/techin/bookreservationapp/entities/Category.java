6971d3ab5250c10a4b92bc9f2bf1c37572cf7c80
Julius <jzabulenas@protonmail.com>
2024-03-23 16:12:14 +0200

    Add validation order to not null and not empty

--- backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
+++ backend/src/main/java/lt/techin/bookreservationapp/entities/Category.java
@@ -6,20 +6,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lt.techin.bookreservationapp.validation.FirstOrder;
+import lt.techin.bookreservationapp.validation.SecondOrder;
 
 @Entity
 @Table(name = "Categories")
+@GroupSequence({Category.class, FirstOrder.class, SecondOrder.class})
 public class Category {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  @NotNull
-  @NotEmpty
+  @NotNull(groups = FirstOrder.class)
+  @NotEmpty(groups = SecondOrder.class)
   @Column(unique = true)
   @Size(min = 3, max = 50, message = "Length must be between 3 and 50 characters")
   private String name;
---
+++ backend/src/main/java/lt/techin/bookreservationapp/validation/FirstOrder.java
@@ -0,0 +1,3 @@
+package lt.techin.bookreservationapp.validation;
+
+public interface FirstOrder {}
