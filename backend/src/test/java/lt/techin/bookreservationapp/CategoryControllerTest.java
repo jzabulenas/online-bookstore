9a71babb405ea0c5bcfb0fb1d5c2bc82c086a623
Julius <jzabulenas@protonmail.com>
2024-03-23 21:52:43 +0200

    Test for 400 and response message when PUT short category name

--- backend/src/test/java/lt/techin/bookreservationapp/CategoryControllerTest.java
+++ backend/src/test/java/lt/techin/bookreservationapp/CategoryControllerTest.java
@@ -493,6 +493,28 @@ public class CategoryControllerTest {
     then(categoryService).should(never()).save(any(Category.class));
   }
 
+  @Test
+  @WithMockUser(roles = {"ADMIN"})
+  void updateCategory_whenAdminCallsWithShortName_then400AndMessage() throws Exception {
+    mockMvc
+        .perform(
+            put("/categories/{id}", 50)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    """
+                		{
+                		  "name": "Ab"
+                		}
+                		""")
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("name").value("Length must be between 3 and 50 characters"));
+
+    then(categoryService).should(never()).findById(anyInt());
+    then(categoryService).should(never()).existsByName(anyString());
+    then(categoryService).should(never()).save(any(Category.class));
+  }
+
   // deleteCategory
 
   @Test
