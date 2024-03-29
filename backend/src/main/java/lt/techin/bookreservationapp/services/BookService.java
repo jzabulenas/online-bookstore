9428af7802cc7e605e134cbb0e8e228682fb7bc9
Julius <jzabulenas@protonmail.com>
2024-03-29 18:43:27 +0200

    Create BookService class

---
+++ backend/src/main/java/lt/techin/bookreservationapp/services/BookService.java
@@ -0,0 +1,35 @@
+package lt.techin.bookreservationapp.services;
+
+import java.util.List;
+
+import lt.techin.bookreservationapp.entities.Book;
+import lt.techin.bookreservationapp.repositories.BookRepository;
+
+public class BookService {
+
+  private final BookRepository bookRepository;
+
+  public BookService(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  public List<Book> findAllBooks() {
+    return bookRepository.findAll();
+  }
+
+  public Book findBookById(int id) {
+    return bookRepository.findById(id).orElseThrow();
+  }
+
+  public boolean existsBookByTitle(String title) {
+    return bookRepository.existsByTitle(title);
+  }
+
+  public boolean existsBookByIsbn(String isbn) {
+    return bookRepository.existsByIsbn(isbn);
+  }
+
+  public Book saveBook(Book book) {
+    return bookRepository.save(book);
+  }
+}
