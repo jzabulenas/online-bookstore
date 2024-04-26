428a4f5af85f253fcfbdf73c1685cf35b9328f0d
Julius <jzabulenas@protonmail.com>
2024-04-26 23:21:10 +0300

    Create 2 books for demo purposes, if not already exist

--- backend/src/main/java/lt/techin/bookreservationapp/runner/DatabaseInitializer.java
+++ backend/src/main/java/lt/techin/bookreservationapp/runner/DatabaseInitializer.java
@@ -59,5 +59,42 @@ public class DatabaseInitializer implements CommandLineRunner {
     if (!categoryService.existsCategoryByName(category2.getName())) {
       categoryService.saveCategory(category2);
     }
+
+    // Books
+    Category categoryFromDb1 = categoryService.findCategoryByName(category1.getName());
+
+    Book book1 =
+        new Book(
+            "The Canterbury Tales: Seventeen Tales and the General Prologue: A Norton Critical Edition (Third Edition) (Norton Critical Editions)",
+            "Geoffrey Chaucer",
+            List.of(categoryFromDb1),
+            "This book has been more helpful to the students—both the better ones and the lesser ones—than any other book I have ever used in any of my classes in my more than a quarter century of university teaching.",
+            "https://m.media-amazon.com/images/I/91sxb8tSpWL._SL1500_.jpg",
+            704,
+            "9781234567897",
+            LocalDate.of(2018, 6, 1),
+            "English");
+
+    if (!bookService.existsBookByIsbn(book1.getIsbn())) {
+      bookService.saveBook(book1);
+    }
+
+    Category categoryFromDb2 = categoryService.findCategoryByName(category2.getName());
+
+    Book book2 =
+        new Book(
+            "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+            "James Clear",
+            List.of(categoryFromDb2),
+            "No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results.",
+            "https://m.media-amazon.com/images/I/81Z+AlCF3tL._SL1500_.jpg",
+            319,
+            "9781234567898",
+            LocalDate.of(2018, 10, 16),
+            "English");
+
+    if (!bookService.existsBookByIsbn(book2.getIsbn())) {
+      bookService.saveBook(book2);
+    }
   }
 }
