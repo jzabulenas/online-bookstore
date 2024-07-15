32e6a02a0ff0d0b367187329170fb925e58eff8a
Julius <jzabulenas@protonmail.com>
2024-07-15 20:37:24 +0300

    Update About

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,15 +1,8 @@
 export default function About() {
   return (
-    <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
-      <div className="col-md-6 p-lg-5 mx-auto my-5">
-        <h3 className="fw-normal text-muted mb-3">Book Reservation App</h3>
-        <ul className="list-unstyled">
-          <li>Add, edit, and delete book categories.</li>
-          <li>Add, edit, and delete a book with assigned categories.</li>
-          <li>Search for a book and reserve it.</li>
-          <li>Add a book to favorites.</li>
-        </ul>
-      </div>
+    <div className="text-center p-5">
+      <h1>Book recommendation app</h1>
+      <p>Easily find new books to read, based on your reading history</p>
     </div>
   );
 }
