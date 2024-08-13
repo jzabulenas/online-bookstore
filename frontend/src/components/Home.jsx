ca647e5e767e8c027afa781a507c5b83e95315bb
Julius <jzabulenas@protonmail.com>
2024-08-13 13:58:11 +0300

    Fetch some user data when person is logged in

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/user", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const result = await response.text();
+
+      setUser(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="text-center p-5">
-      <h1 className="fw-bold text-break">Book recommendation app</h1>
-      <h2 className="fs-5 text-black text-opacity-75">
-        Easily find new books to read
-      </h2>
-      <Link to="/about">Learn more</Link>
-    </div>
+    <>
+      {user ? (
+        <p>Welcome {user}</p>
+      ) : (
+        <div className="text-center p-5">
+          <h1 className="fw-bold text-break">Book recommendation app</h1>
+          <h2 className="fs-5 text-black text-opacity-75">
+            Easily find new books to read
+          </h2>
+          <Link to="/about">Learn more</Link>
+        </div>
+      )}
+    </>
   );
 }
