61a79d09eba81e0863e9ba30890e20647e4d3fc8
Julius <jzabulenas@protonmail.com>
2024-08-14 11:25:44 +0300

    Add route for OAuth2RedirectHandler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Categories from "./components/Categories";
-import Books from "./components/Books";
-import FavoriteBooks from "./components/FavoriteBooks";
-import ReservedBooks from "./components/ReservedBooks";
 import About from "./components/About";
+import Books from "./components/Books";
+import Categories from "./components/Categories";
 import ErrorPage from "./components/ErrorPage";
+import FavoriteBooks from "./components/FavoriteBooks";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import OAuth2RedirectHandler from "./components/OAuth2RedirectHandler";
+import ReservedBooks from "./components/ReservedBooks";
 import Signup from "./components/Signup";
 
 function App() {
@@ -52,6 +53,10 @@ function App() {
           <Route
             path="/signup"
             element={<Signup />}
+          />
+          <Route
+            path="/oauth2/redirect"
+            element={<OAuth2RedirectHandler />}
           />
         </Routes>
       </main>
