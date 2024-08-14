b722ac43ebba384f267eead4640791e8fb4522f0
Julius <jzabulenas@protonmail.com>
2024-08-14 11:23:00 +0300

    Add OAuth2RedirectHandler

---
+++ frontend/src/components/OAuth2RedirectHandler.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function OAuthRedirectHandler() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/user", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const result = await response.text();
+        sessionStorage.setItem("user", result);
+        navigate("/"); // Redirect to home
+      } else {
+        console.error("Failed to fetch user data");
+        navigate("/login"); // Redirect back to login if needed
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+
+  return <p>Redirecting...</p>;
+}
