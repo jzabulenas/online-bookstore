645b306b1c89f5f843f21b1bb88f18c7227099c6
Julius <jzabulenas@protonmail.com>
2024-08-17 15:15:06 +0300

    Add a dispatchEvent for storage to OAuth2RedirectHandler

--- frontend/src/components/OAuth2RedirectHandler.jsx
+++ frontend/src/components/OAuth2RedirectHandler.jsx
@@ -16,6 +16,7 @@ export default function OAuth2RedirectHandler() {
         sessionStorage.setItem("email", result.name);
         sessionStorage.setItem("roles", JSON.stringify(result.authorities));
         navigate("/"); // Redirect to home
+        window.dispatchEvent(new Event("storage")); // Trigger a storage event manually
       } else {
         console.error("Failed to fetch user data");
         navigate("/login"); // Redirect back to login if needed
