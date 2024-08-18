5ab267663b54efc646983363bd27018bd13cbc39
Julius <jzabulenas@protonmail.com>
2024-08-18 16:24:35 +0300

    Disable redirect on logout

--- backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -75,6 +76,8 @@ public class SecurityConfig {
               oauth2.userInfoEndpoint(c -> c.userService(customOAuth2UserService));
               oauth2.successHandler(oAuth2LoginSuccessHandler);
             })
+        .logout(
+            (logout) -> logout.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
         .build();
   }
 
