72630af1160caf054618a8b48f53181d316b70ce
Julius <jzabulenas@protonmail.com>
2024-08-31 11:09:38 +0300

    Add method to query LLM

--- backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
@@ -1,6 +1,23 @@
 package lt.techin.bookreservationapp.controllers;
 
+import java.util.Map;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BookController {}
+public class BookController {
+
+  private final ChatClient chatClient;
+
+  public BookController(ChatClient chatClient) {
+    this.chatClient = chatClient;
+  }
+
+  @PostMapping("/generate-books")
+  public Map<String, String> generateBooks(@RequestBody String message) {
+    return Map.of("result", chatClient.prompt().user(message).call().content());
+  }
+}
