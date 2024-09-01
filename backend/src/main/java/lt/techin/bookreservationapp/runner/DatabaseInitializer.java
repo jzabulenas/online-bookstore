fe24695095a6b80984fda7833f0143e6ccc9bcf1
Julius <jzabulenas@protonmail.com>
2024-09-01 13:59:54 +0300

    Remove not used classes from DatabaseInitializer

--- backend/src/main/java/lt/techin/bookreservationapp/runner/DatabaseInitializer.java
+++ backend/src/main/java/lt/techin/bookreservationapp/runner/DatabaseInitializer.java
@@ -1,16 +1,9 @@
 package lt.techin.bookreservationapp.runner;
 
-import java.time.LocalDate;
-import java.util.Set;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import lt.techin.bookreservationapp.entities.Book;
-import lt.techin.bookreservationapp.entities.Category;
-import lt.techin.bookreservationapp.services.BookService;
-import lt.techin.bookreservationapp.services.CategoryService;
 import lt.techin.bookreservationapp.services.UserService;
 
 @Component
@@ -20,15 +13,14 @@ public class DatabaseInitializer implements CommandLineRunner {
 
   private final UserService userService;
 
-  private final CategoryService categoryService;
+  // private final CategoryService categoryService;
 
-  private final BookService bookService;
+  // private final BookService bookService;
 
-  public DatabaseInitializer(
-      UserService userService, CategoryService categoryService, BookService bookService) {
+  public DatabaseInitializer(UserService userService) {
     this.userService = userService;
-    this.categoryService = categoryService;
-    this.bookService = bookService;
+    // this.categoryService = categoryService;
+    // this.bookService = bookService;
   }
 
   @Override
@@ -54,51 +46,57 @@ public class DatabaseInitializer implements CommandLineRunner {
     //    }
 
     // Categories
-    Category category1 = new Category("History");
-    if (!categoryService.existsCategoryByName(category1.getName())) {
-      categoryService.saveCategory(category1);
-    }
+    //    Category category1 = new Category("History");
+    //    if (!categoryService.existsCategoryByName(category1.getName())) {
+    //      categoryService.saveCategory(category1);
+    //    }
 
-    Category category2 = new Category("Self-Help");
-    if (!categoryService.existsCategoryByName(category2.getName())) {
-      categoryService.saveCategory(category2);
-    }
+    //    Category category2 = new Category("Self-Help");
+    //    if (!categoryService.existsCategoryByName(category2.getName())) {
+    //      categoryService.saveCategory(category2);
+    //    }
 
     // Books
-    Category categoryFromDb1 = categoryService.findCategoryByName(category1.getName());
+    //    Category categoryFromDb1 = categoryService.findCategoryByName(category1.getName());
 
-    Book book1 =
-        new Book(
-            "The Canterbury Tales: Seventeen Tales and the General Prologue: A Norton Critical Edition (Third Edition) (Norton Critical Editions)",
-            "Geoffrey Chaucer",
-            Set.of(categoryFromDb1),
-            "This book has been more helpful to the students—both the better ones and the lesser ones—than any other book I have ever used in any of my classes in my more than a quarter century of university teaching.",
-            "https://m.media-amazon.com/images/I/91sxb8tSpWL._SL1500_.jpg",
-            704,
-            "9781234567897",
-            LocalDate.of(2018, 6, 1),
-            "English");
+    //    Book book1 =
+    //        new Book(
+    //            "The Canterbury Tales: Seventeen Tales and the General Prologue: A Norton Critical
+    // Edition (Third Edition) (Norton Critical Editions)",
+    //            "Geoffrey Chaucer",
+    //            Set.of(categoryFromDb1),
+    //            "This book has been more helpful to the students—both the better ones and the
+    // lesser ones—than any other book I have ever used in any of my classes in my more than a
+    // quarter century of university teaching.",
+    //            "https://m.media-amazon.com/images/I/91sxb8tSpWL._SL1500_.jpg",
+    //            704,
+    //            "9781234567897",
+    //            LocalDate.of(2018, 6, 1),
+    //            "English");
 
-    if (!bookService.existsBookByIsbn(book1.getIsbn())) {
-      bookService.saveBook(book1);
-    }
+    //    if (!bookService.existsBookByIsbn(book1.getIsbn())) {
+    //      bookService.saveBook(book1);
+    //    }
 
-    Category categoryFromDb2 = categoryService.findCategoryByName(category2.getName());
+    //    Category categoryFromDb2 = categoryService.findCategoryByName(category2.getName());
 
-    Book book2 =
-        new Book(
-            "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-            "James Clear",
-            Set.of(categoryFromDb2),
-            "No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results.",
-            "https://m.media-amazon.com/images/I/81Z+AlCF3tL._SL1500_.jpg",
-            319,
-            "9781234567898",
-            LocalDate.of(2018, 10, 16),
-            "English");
+    //    Book book2 =
+    //        new Book(
+    //            "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    //            "James Clear",
+    //            Set.of(categoryFromDb2),
+    //            "No matter your goals, Atomic Habits offers a proven framework for
+    // improving--every day. James Clear, one of the world's leading experts on habit formation,
+    // reveals practical strategies that will teach you exactly how to form good habits, break bad
+    // ones, and master the tiny behaviors that lead to remarkable results.",
+    //            "https://m.media-amazon.com/images/I/81Z+AlCF3tL._SL1500_.jpg",
+    //            319,
+    //            "9781234567898",
+    //            LocalDate.of(2018, 10, 16),
+    //            "English");
 
-    if (!bookService.existsBookByIsbn(book2.getIsbn())) {
-      bookService.saveBook(book2);
-    }
+    //    if (!bookService.existsBookByIsbn(book2.getIsbn())) {
+    //      bookService.saveBook(book2);
+    //    }
   }
 }
