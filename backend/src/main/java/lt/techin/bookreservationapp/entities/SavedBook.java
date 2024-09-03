69ae78e740b6f1e7e49b149144b42f1da2e494ea
Julius <jzabulenas@protonmail.com>
2024-09-03 20:03:51 +0300

    Create constructor, remove setters from SavedBook

--- backend/src/main/java/lt/techin/bookreservationapp/entities/SavedBook.java
+++ backend/src/main/java/lt/techin/bookreservationapp/entities/SavedBook.java
@@ -20,19 +20,16 @@ public class SavedBook {
 
   @NotNull @ManyToOne private User user;
 
+  public SavedBook(String title, User user) {
+    this.title = title;
+    this.user = user;
+  }
+
   public String getTitle() {
     return title;
   }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
   public User getUser() {
     return user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
   }
 }
