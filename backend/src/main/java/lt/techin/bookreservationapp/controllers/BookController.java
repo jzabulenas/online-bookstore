9d367ae6ad4227f831a4a7783e9f0db34b73d1ae
Julius <jzabulenas@protonmail.com>
2024-09-05 20:52:56 +0300

    Update generateBooks method to not include saved books in new generation result

--- backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
+++ backend/src/main/java/lt/techin/bookreservationapp/controllers/BookController.java
@@ -1,27 +1,37 @@
 package lt.techin.bookreservationapp.controllers;
 
+import java.util.List;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lt.techin.bookreservationapp.repositories.SavedBookRepository;
+
 @RestController
 public class BookController {
 
   private final ChatClient chatClient;
+  private final SavedBookRepository savedBookRepository;
 
-  public BookController(ChatClient chatClient) {
+  public BookController(ChatClient chatClient, SavedBookRepository savedBookRepository) {
     this.chatClient = chatClient;
+    this.savedBookRepository = savedBookRepository;
   }
 
   @PostMapping("/generate-books")
   public String generateBooks(@RequestBody String message) {
+    List<String> titles = this.savedBookRepository.findAllTitles();
+
     return chatClient
         .prompt()
         .user(
             "I have read "
                 + message
-                + " and liked it. Suggest me 3 new books to read. Only provide title, and author. It should adhere this format: \"Book name by Author\". The result should be stored in a JavaScript array. Do not provide any introduction, like \"Here are three...\"")
+                + " and liked it. Suggest me 3 new books to read. Only provide title, and author. It should adhere this format: \"Book name by Author\". The result should be stored in a JavaScript array. Do not provide any introduction, like \"Here are three...\""
+                + " Also make sure to not include these books in the result: "
+                + titles)
         .call()
         .content();
   }
