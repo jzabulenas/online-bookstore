b6d17d9c6b93e175e60c91867ccea78313447cdd
Julius <jzabulenas@protonmail.com>
2025-02-27 09:01:49 +0200

    Move UserDetailsServiceImpl class to security package

--- backend/src/main/java/lt/techin/bookreservationapp/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/UserDetailsServiceImpl.java
@@ -1,28 +1,23 @@
-// package lt.techin.bookreservationapp.security;
+package lt.techin.bookreservationapp.security;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// import lt.techin.bookreservationapp.entities.User;
-// import lt.techin.bookreservationapp.repositories.UserRepository;
+import lt.techin.bookreservationapp.repositories.UserRepository;
 
-// @Service
-// public class UserDetailsServiceImpl implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-//  private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-//  @Autowired
-//  public UserDetailsServiceImpl(UserRepository userRepository) {
-//    this.userRepository = userRepository;
-//  }
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-//  @Override
-//  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//    User user = userRepository.findUserByUsername(username).get();
-
-//    return user;
-//  }
-// }
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return this.userRepository.findByEmail(username).orElseThrow();
+  }
+}
