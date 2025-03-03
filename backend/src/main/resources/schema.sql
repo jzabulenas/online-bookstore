0897651351c8fa44c79620686ece4217b421715a
Julius <jzabulenas@protonmail.com>
2025-03-03 09:28:06 +0200

    Update schema.sql

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,6 +1,35 @@
-CREATE TABLE `saved_books` (
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `books` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKqv8dy63m3nvdya1slwm78okld` (`title`,`user_id`),
+  KEY `FKcqud6407tfhc3y3dv60eevw9d` (`user_id`),
+  CONSTRAINT `FKcqud6407tfhc3y3dv60eevw9d` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
+);
+
+INSERT INTO roles (name)
+VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+
+-- TODO: maybe make DDL less specific, just foreign keys and what not?
+CREATE TABLE `users_roles` (
+  `user_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  UNIQUE KEY `user_id` (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
 );
