e761dc2c6cbd39ed43e5419d55eff61268f788e8
Julius <jzabulenas@protonmail.com>
2025-03-20 10:11:05 +0200

    Add start of log in form in frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,46 @@
+import { useForm } from "react-hook-form";
+
 export default function Login() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="row">
-      <div className="col">
-        <a href="http://localhost:8080/login">Log in</a>
+    <>
+      <div className="row">
+        <div className="col">
+          <a href="http://localhost:8080/login">Log in</a>
+        </div>
       </div>
-    </div>
+
+      <div className="row">
+        <div className="col-4 mx-auto">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+              <label
+                htmlFor="email"
+                className="form-label"
+              >
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="form-control"
+                {...register("email")}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
