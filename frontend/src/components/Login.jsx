fb28bd6b66ecfd92ac5333acb8b8a97b7c5866e3
Julius <jzabulenas@protonmail.com>
2025-03-22 20:39:49 +0200

    Add onSubmit to Login, currently not working

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,46 @@ import { useForm } from "react-hook-form";
 export default function Login() {
   const { register, handleSubmit } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    async function postData() {
+      const url = "http://localhost:8080/ott/generate";
+
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            // "X-XSRF-TOKEN": csrfToken(),
+          },
+          body: new URLSearchParams({
+            username: data.email,
+          }).toString(),
+          redirect: "follow",
+        });
+
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        // const json = await response.json();
+        // setBooks(json);
+        // console.log(json);
+        console.log(
+          new URLSearchParams({
+            username: data.email,
+          }).toString()
+        );
+        console.log(response);
+      } catch (error) {
+        // console.error(error.message);
+        // sessionStorage.clear();
+        // navigate("/login");
+      }
+    }
+
+    postData();
+  };
 
   return (
     <>
