5f52d39392b502da28edc2b6a0b6bb68d2c321bb
Julius <jzabulenas@protonmail.com>
2025-03-25 22:52:53 +0200

    Migrate backend back to basic auth (still need to fix tests)

--- backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
@@ -1,149 +1,141 @@
 package lt.techin.bookreservationapp.security;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lt.techin.bookreservationapp.role.Role;
 import lt.techin.bookreservationapp.role.RoleRepository;
-import lt.techin.bookreservationapp.user.User;
 import lt.techin.bookreservationapp.user.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  // private final CustomOAuth2UserService customOAuth2UserService;
-  // private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
   private final String frontendUrl;
   private final UserRepository userRepository;
   private final RoleRepository roleRepository;
 
   SecurityConfig(
-      // CustomOAuth2UserService customOAuth2UserService,
-      // OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler,
       @Value("${frontend.url}") String frontendUrl,
       UserRepository userRepository,
       RoleRepository roleRepository) {
-
-    // this.customOAuth2UserService = customOAuth2UserService;
-    // this.oAuth2LoginSuccessHandler = oAuth2LoginSuccessHandler;
     this.frontendUrl = frontendUrl;
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
   }
 
-  // @Bean
-  // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-  // Exception {
-  // http.authorizeHttpRequests(
-  // authorize ->
-  // authorize
-  // .requestMatchers("/signup")
-  // .permitAll()
-  // .requestMatchers("/login")
-  // .permitAll()
-  // .requestMatchers("/h2-console/**")
-  // .permitAll()
-  // .requestMatchers(HttpMethod.POST, "/categories")
-  // .hasRole("ADMIN")
-  // .requestMatchers(HttpMethod.PUT, "/categories/**")
-  // .hasRole("ADMIN")
-  // .requestMatchers(HttpMethod.DELETE, "/categories/**")
-  // .hasRole("ADMIN")
-  // .anyRequest()
-  // .authenticated())
-  // .headers(headers -> headers.frameOptions(f -> f.disable()))
-  // .csrf(csrf -> csrf.disable())
-  // .httpBasic(Customizer.withDefaults());
-  //
-  // return http.build();
-  // }
-
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    return http.csrf(
-            (csrf) ->
-                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
-        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        .authorizeHttpRequests(
-            authorize -> {
-              authorize.anyRequest().authenticated();
-            })
-        .formLogin(Customizer.withDefaults())
-        .oneTimeTokenLogin(
-            configurer -> {
-              configurer.tokenGenerationSuccessHandler(
-                  (request, response, oneTimeToken) -> {
-                    System.out.println(oneTimeToken.getUsername());
+    http.csrf(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
 
-                    var token = oneTimeToken.getTokenValue();
+    return http.build();
 
-                    var msg = "Please go to http://localhost:8080/login/ott?token=" + token;
-                    System.out.println(msg);
-
-                    response.setContentType(MediaType.TEXT_HTML_VALUE);
-                    response.getWriter().write("You've got console mail!");
-
-                    if (!this.userRepository.existsByEmail(oneTimeToken.getUsername())) {
-                      Optional<Role> role = this.roleRepository.findByName("ROLE_USER");
-
-                      User user = new User();
-                      user.setEmail(oneTimeToken.getUsername());
-                      user.setRoles(List.of(role.get()));
-                      this.userRepository.save(user);
-                    }
-                  });
-              // configurer.tokenService(
-              // new OneTimeTokenService() {
-              //
-              // @Override
-              // public OneTimeToken generate(GenerateOneTimeTokenRequest
-              // request) {
-              //
-              // return null;
-              // }
-              //
-              // @Override
-              // public OneTimeToken consume(
-              // OneTimeTokenAuthenticationToken authenticationToken) {
-              //
-              // return null;
-              // }
-              // });
-              configurer.authenticationSuccessHandler(
-                  (request, response, authentication) -> {
-                    response.sendRedirect(this.frontendUrl + "/oauth2/redirect");
-                  });
-            })
-
-        // .oauth2Login(
-        // oauth2 -> {
-        // oauth2.loginPage("http://localhost:5173/login").permitAll();
-        // oauth2.userInfoEndpoint(c -> c.userService(customOAuth2UserService));
-        // oauth2.successHandler(oAuth2LoginSuccessHandler);
-        // })
-        // .logout(
-        // (logout) -> logout.logoutSuccessHandler(new
-        // HttpStatusReturningLogoutSuccessHandler()))
-        .build();
+    //    http.authorizeHttpRequests(
+    //            authorize ->
+    //                authorize
+    //                    .requestMatchers("/signup")
+    //                    .permitAll()
+    //                    .requestMatchers("/login")
+    //                    .permitAll()
+    //                    .requestMatchers("/h2-console/**")
+    //                    .permitAll()
+    //                    .requestMatchers(HttpMethod.POST, "/categories")
+    //                    .hasRole("ADMIN")
+    //                    .requestMatchers(HttpMethod.PUT, "/categories/**")
+    //                    .hasRole("ADMIN")
+    //                    .requestMatchers(HttpMethod.DELETE, "/categories/**")
+    //                    .hasRole("ADMIN")
+    //                    .anyRequest()
+    //                    .authenticated())
+    //        .csrf(csrf -> csrf.disable())
+    //        .httpBasic();
+    //
+    //    return http.build();
   }
+
+  //  @Bean
+  //  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  //
+  //    return http.csrf(
+  //            (csrf) ->
+  //                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+  //                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+  //        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+  //        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+  //        .authorizeHttpRequests(
+  //            authorize -> {
+  //              authorize.anyRequest().authenticated();
+  //            })
+  //        .formLogin(Customizer.withDefaults())
+  //        .oneTimeTokenLogin(
+  //            configurer -> {
+  //              configurer.tokenGenerationSuccessHandler(
+  //                  (request, response, oneTimeToken) -> {
+  //                    System.out.println(oneTimeToken.getUsername());
+  //
+  //                    var token = oneTimeToken.getTokenValue();
+  //
+  //                    var msg = "Please go to http://localhost:8080/login/ott?token=" + token;
+  //                    System.out.println(msg);
+  //
+  //                    response.setContentType(MediaType.TEXT_HTML_VALUE);
+  //                    response.getWriter().write("You've got console mail!");
+  //
+  //                    if (!this.userRepository.existsByEmail(oneTimeToken.getUsername())) {
+  //                      Optional<Role> role = this.roleRepository.findByName("ROLE_USER");
+  //
+  //                      User user = new User();
+  //                      user.setEmail(oneTimeToken.getUsername());
+  //                      user.setRoles(List.of(role.get()));
+  //                      this.userRepository.save(user);
+  //                    }
+  //                  });
+  //              // configurer.tokenService(
+  //              // new OneTimeTokenService() {
+  //              //
+  //              // @Override
+  //              // public OneTimeToken generate(GenerateOneTimeTokenRequest
+  //              // request) {
+  //              //
+  //              // return null;
+  //              // }
+  //              //
+  //              // @Override
+  //              // public OneTimeToken consume(
+  //              // OneTimeTokenAuthenticationToken authenticationToken) {
+  //              //
+  //              // return null;
+  //              // }
+  //              // });
+  //              configurer.authenticationSuccessHandler(
+  //                  (request, response, authentication) -> {
+  //                    response.sendRedirect(this.frontendUrl + "/oauth2/redirect");
+  //                  });
+  //            })
+  //
+  //        // .oauth2Login(
+  //        // oauth2 -> {
+  //        // oauth2.loginPage("http://localhost:5173/login").permitAll();
+  //        // oauth2.userInfoEndpoint(c -> c.userService(customOAuth2UserService));
+  //        // oauth2.successHandler(oAuth2LoginSuccessHandler);
+  //        // })
+  //        // .logout(
+  //        // (logout) -> logout.logoutSuccessHandler(new
+  //        // HttpStatusReturningLogoutSuccessHandler()))
+  //        .build();
+  //  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
