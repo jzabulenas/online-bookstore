eb6ec83728dfcdb0b16f1d8b5bc84890ab8d808c
Julius <jzabulenas@protonmail.com>
2025-03-25 23:00:34 +0200

    Add PasswordEncoder bean

--- backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -154,5 +156,10 @@ public class SecurityConfig {
     urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
 
     return urlBasedCorsConfigurationSource;
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
