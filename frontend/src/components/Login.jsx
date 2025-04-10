8f44794ea7f274d649b74fedf724ff0748779c66
Julius <jzabulenas@protonmail.com>
2025-04-10 22:19:29 +0300

    Add functionality for log in

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,12 @@
 import { useForm } from "react-hook-form";
+import csrfToken from "../util/getCsrfToken";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     async function postData() {
-      const url = "http://localhost:8080/ott/generate";
+      const url = "http://localhost:8080/login";
 
       try {
         const response = await fetch(url, {
@@ -13,12 +14,13 @@ export default function Login() {
           credentials: "include",
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            // "X-XSRF-TOKEN": csrfToken(),
+            "X-XSRF-TOKEN": csrfToken(),
           },
           body: new URLSearchParams({
             username: data.email,
+            password: data.password,
           }).toString(),
-          redirect: "follow",
+          // redirect: "follow",
         });
 
         if (!response.ok) {
@@ -28,14 +30,16 @@ export default function Login() {
         // const json = await response.json();
         // setBooks(json);
         // console.log(json);
-        console.log(
-          new URLSearchParams({
-            username: data.email,
-          }).toString()
-        );
+        // console.log(
+        //   new URLSearchParams({
+        //     username: data.email,
+        //     password: data.password,
+        //   }).toString()
+        // );
+
         console.log(response);
       } catch (error) {
-        // console.error(error.message);
+        console.error(error.message);
         // sessionStorage.clear();
         // navigate("/login");
       }
@@ -46,11 +50,11 @@ export default function Login() {
 
   return (
     <>
-      <div className="row">
+      {/* <div className="row">
         <div className="col">
           <a href="http://localhost:8080/login">Log in</a>
         </div>
-      </div>
+      </div> */}
 
       <div className="row">
         <div className="col-4 mx-auto">
@@ -68,6 +72,22 @@ export default function Login() {
                 id="email"
                 className="form-control"
                 {...register("email")}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label
+                htmlFor="password"
+                className="form-label"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="form-control"
+                {...register("password")}
               />
             </div>
 
