16e741d548c89ce2f1458eec6e1815e1d132fe52
Julius <jzabulenas@protonmail.com>
2025-04-10 22:22:07 +0300

    Remove not needed comments from Login component

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,28 +20,13 @@ export default function Login() {
             username: data.email,
             password: data.password,
           }).toString(),
-          // redirect: "follow",
         });
 
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-
-        // const json = await response.json();
-        // setBooks(json);
-        // console.log(json);
-        // console.log(
-        //   new URLSearchParams({
-        //     username: data.email,
-        //     password: data.password,
-        //   }).toString()
-        // );
-
-        console.log(response);
       } catch (error) {
         console.error(error.message);
-        // sessionStorage.clear();
-        // navigate("/login");
       }
     }
 
@@ -50,12 +35,6 @@ export default function Login() {
 
   return (
     <>
-      {/* <div className="row">
-        <div className="col">
-          <a href="http://localhost:8080/login">Log in</a>
-        </div>
-      </div> */}
-
       <div className="row">
         <div className="col">
           <form onSubmit={handleSubmit(onSubmit)}>
