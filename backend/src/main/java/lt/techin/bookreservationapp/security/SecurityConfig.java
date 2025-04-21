cc3dafbd5f23be3e35f18b7cc50cb75761cbd0e8
Julius <jzabulenas@protonmail.com>
2025-04-21 13:55:37 +0300

    Format codebase with formatter

--- backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
+++ backend/src/main/java/lt/techin/bookreservationapp/security/SecurityConfig.java
@@ -56,100 +56,100 @@ public class SecurityConfig {
 
     return http.build();
 
-    //    http.authorizeHttpRequests(
-    //            authorize ->
-    //                authorize
-    //                    .requestMatchers("/signup")
-    //                    .permitAll()
-    //                    .requestMatchers("/login")
-    //                    .permitAll()
-    //                    .requestMatchers("/h2-console/**")
-    //                    .permitAll()
-    //                    .requestMatchers(HttpMethod.POST, "/categories")
-    //                    .hasRole("ADMIN")
-    //                    .requestMatchers(HttpMethod.PUT, "/categories/**")
-    //                    .hasRole("ADMIN")
-    //                    .requestMatchers(HttpMethod.DELETE, "/categories/**")
-    //                    .hasRole("ADMIN")
-    //                    .anyRequest()
-    //                    .authenticated())
-    //        .csrf(csrf -> csrf.disable())
-    //        .httpBasic();
+    // http.authorizeHttpRequests(
+    // authorize ->
+    // authorize
+    // .requestMatchers("/signup")
+    // .permitAll()
+    // .requestMatchers("/login")
+    // .permitAll()
+    // .requestMatchers("/h2-console/**")
+    // .permitAll()
+    // .requestMatchers(HttpMethod.POST, "/categories")
+    // .hasRole("ADMIN")
+    // .requestMatchers(HttpMethod.PUT, "/categories/**")
+    // .hasRole("ADMIN")
+    // .requestMatchers(HttpMethod.DELETE, "/categories/**")
+    // .hasRole("ADMIN")
+    // .anyRequest()
+    // .authenticated())
+    // .csrf(csrf -> csrf.disable())
+    // .httpBasic();
     //
-    //    return http.build();
+    // return http.build();
   }
 
-  //  @Bean
-  //  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  // @Bean
+  // SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
   //
-  //    return http.csrf(
-  //            (csrf) ->
-  //                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-  //                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
-  //        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-  //        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-  //        .authorizeHttpRequests(
-  //            authorize -> {
-  //              authorize.anyRequest().authenticated();
-  //            })
-  //        .formLogin(Customizer.withDefaults())
-  //        .oneTimeTokenLogin(
-  //            configurer -> {
-  //              configurer.tokenGenerationSuccessHandler(
-  //                  (request, response, oneTimeToken) -> {
-  //                    System.out.println(oneTimeToken.getUsername());
+  // return http.csrf(
+  // (csrf) ->
+  // csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+  // .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+  // .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+  // .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+  // .authorizeHttpRequests(
+  // authorize -> {
+  // authorize.anyRequest().authenticated();
+  // })
+  // .formLogin(Customizer.withDefaults())
+  // .oneTimeTokenLogin(
+  // configurer -> {
+  // configurer.tokenGenerationSuccessHandler(
+  // (request, response, oneTimeToken) -> {
+  // System.out.println(oneTimeToken.getUsername());
   //
-  //                    var token = oneTimeToken.getTokenValue();
+  // var token = oneTimeToken.getTokenValue();
   //
-  //                    var msg = "Please go to http://localhost:8080/login/ott?token=" + token;
-  //                    System.out.println(msg);
+  // var msg = "Please go to http://localhost:8080/login/ott?token=" + token;
+  // System.out.println(msg);
   //
-  //                    response.setContentType(MediaType.TEXT_HTML_VALUE);
-  //                    response.getWriter().write("You've got console mail!");
+  // response.setContentType(MediaType.TEXT_HTML_VALUE);
+  // response.getWriter().write("You've got console mail!");
   //
-  //                    if (!this.userRepository.existsByEmail(oneTimeToken.getUsername())) {
-  //                      Optional<Role> role = this.roleRepository.findByName("ROLE_USER");
+  // if (!this.userRepository.existsByEmail(oneTimeToken.getUsername())) {
+  // Optional<Role> role = this.roleRepository.findByName("ROLE_USER");
   //
-  //                      User user = new User();
-  //                      user.setEmail(oneTimeToken.getUsername());
-  //                      user.setRoles(List.of(role.get()));
-  //                      this.userRepository.save(user);
-  //                    }
-  //                  });
-  //              // configurer.tokenService(
-  //              // new OneTimeTokenService() {
-  //              //
-  //              // @Override
-  //              // public OneTimeToken generate(GenerateOneTimeTokenRequest
-  //              // request) {
-  //              //
-  //              // return null;
-  //              // }
-  //              //
-  //              // @Override
-  //              // public OneTimeToken consume(
-  //              // OneTimeTokenAuthenticationToken authenticationToken) {
-  //              //
-  //              // return null;
-  //              // }
-  //              // });
-  //              configurer.authenticationSuccessHandler(
-  //                  (request, response, authentication) -> {
-  //                    response.sendRedirect(this.frontendUrl + "/oauth2/redirect");
-  //                  });
-  //            })
+  // User user = new User();
+  // user.setEmail(oneTimeToken.getUsername());
+  // user.setRoles(List.of(role.get()));
+  // this.userRepository.save(user);
+  // }
+  // });
+  // // configurer.tokenService(
+  // // new OneTimeTokenService() {
+  // //
+  // // @Override
+  // // public OneTimeToken generate(GenerateOneTimeTokenRequest
+  // // request) {
+  // //
+  // // return null;
+  // // }
+  // //
+  // // @Override
+  // // public OneTimeToken consume(
+  // // OneTimeTokenAuthenticationToken authenticationToken) {
+  // //
+  // // return null;
+  // // }
+  // // });
+  // configurer.authenticationSuccessHandler(
+  // (request, response, authentication) -> {
+  // response.sendRedirect(this.frontendUrl + "/oauth2/redirect");
+  // });
+  // })
   //
-  //        // .oauth2Login(
-  //        // oauth2 -> {
-  //        // oauth2.loginPage("http://localhost:5173/login").permitAll();
-  //        // oauth2.userInfoEndpoint(c -> c.userService(customOAuth2UserService));
-  //        // oauth2.successHandler(oAuth2LoginSuccessHandler);
-  //        // })
-  //        // .logout(
-  //        // (logout) -> logout.logoutSuccessHandler(new
-  //        // HttpStatusReturningLogoutSuccessHandler()))
-  //        .build();
-  //  }
+  // // .oauth2Login(
+  // // oauth2 -> {
+  // // oauth2.loginPage("http://localhost:5173/login").permitAll();
+  // // oauth2.userInfoEndpoint(c -> c.userService(customOAuth2UserService));
+  // // oauth2.successHandler(oAuth2LoginSuccessHandler);
+  // // })
+  // // .logout(
+  // // (logout) -> logout.logoutSuccessHandler(new
+  // // HttpStatusReturningLogoutSuccessHandler()))
+  // .build();
+  // }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
