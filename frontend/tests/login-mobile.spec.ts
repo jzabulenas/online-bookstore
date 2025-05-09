c8d3247fa0ef31304d97b9f1a17d30be7d6dfdd6
Julius <jzabulenas@protonmail.com>
2025-05-09 11:23:42 +0300

    Update mobile log in test

--- frontend/tests/login-mobile.spec.ts
+++ frontend/tests/login-mobile.spec.ts
@@ -4,18 +4,17 @@ test.use({
   ...devices["Pixel 5"],
 });
 
-// TODO: Perhaps not .click but .tap?. Also, I see Tap; there is no tab on mobile
 test("should display error message, when log in credentials are incorrect", async ({
   page,
 }) => {
   await page.goto("http://localhost:5173/");
-  await page.getByRole("button", { name: "Toggle navigation" }).click();
-  await page.getByRole("link", { name: "Log in" }).click();
-  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("button", { name: "Toggle navigation" }).tap();
+  await page.getByRole("link", { name: "Log in" }).tap();
+  await page.getByRole("textbox", { name: "Email:" }).tap();
   await page.getByRole("textbox", { name: "Email:" }).fill("jurgis@inbox.lt");
-  await page.getByRole("textbox", { name: "Email:" }).press("Tab");
+  await page.getByRole("textbox", { name: "Email:" }).tap();
   await page.getByRole("textbox", { name: "Password:" }).fill("12345");
-  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Submit" }).tap();
   await expect(page.getByText("Username or password is")).toBeVisible();
   await expect(page.getByRole("paragraph")).toContainText(
     "Username or password is incorrect."
