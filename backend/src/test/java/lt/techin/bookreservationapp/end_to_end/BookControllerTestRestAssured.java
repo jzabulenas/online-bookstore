38d135f5041197e1a69b92d8175992648adec883
Julius <jzabulenas@protonmail.com>
2025-05-18 23:30:23 +0300

    Add test "generateBooks_whenBookIsGenerated_return200AndListOfBooks"

--- backend/src/test/java/lt/techin/bookreservationapp/end_to_end/BookControllerTestRestAssured.java
+++ backend/src/test/java/lt/techin/bookreservationapp/end_to_end/BookControllerTestRestAssured.java
@@ -22,11 +22,15 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import lt.techin.bookreservationapp.book.Book;
 import lt.techin.bookreservationapp.book.BookRepository;
+import lt.techin.bookreservationapp.book.MessageRequestDTO;
 import lt.techin.bookreservationapp.role.Role;
 import lt.techin.bookreservationapp.role.RoleRepository;
 import lt.techin.bookreservationapp.user.User;
@@ -70,7 +74,60 @@ class BookControllerTestRestAssured {
     this.userRepository.deleteAll();
   }
 
+  // generateBooks
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+
+  @Test
+  void generateBooks_whenBookIsGenerated_return200AndListOfBooks() throws JsonProcessingException {
+    Optional<Role> role = this.roleRepository.findByName("ROLE_USER");
+
+    User user =
+        this.userRepository.save(
+            new User(
+                "jurgis@inbox.lt", passwordEncoder.encode("123456"), List.of(role.orElseThrow())));
+
+    Response csrfResponse = given().when().get("/open").then().extract().response();
+
+    String csrfToken = csrfResponse.cookie("XSRF-TOKEN");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    Response response =
+        given()
+            .cookie("XSRF-TOKEN", csrfToken)
+            .header("X-XSRF-TOKEN", csrfToken)
+            .contentType(ContentType.URLENC)
+            .body("username=jurgis%40inbox.lt&password=123456")
+            .post("/login")
+            .then()
+            .statusCode(200)
+            .extract()
+            .response();
+
+    given()
+        .cookie("JSESSIONID", response.getSessionId())
+        .cookie("XSRF-TOKEN", csrfToken)
+        .header("X-XSRF-TOKEN", csrfToken)
+        .contentType(ContentType.JSON)
+        .body(objectMapper.writeValueAsString(new MessageRequestDTO("Dracula by Bram Stoker")))
+        .post("/generate-books")
+        .then()
+        .statusCode(200)
+        .body(".", aMapWithSize(1))
+        .body("result", hasSize(3));
   }
+
+  // TODO: test to check if same books are not generated as previously?
+
+  //
 
   @Test
   void getBooks_whenAuthenticated_returnBooksAnd200() {
