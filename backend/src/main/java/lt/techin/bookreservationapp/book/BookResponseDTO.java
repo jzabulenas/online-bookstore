ef2581ead33b3c465277b2ae45a5f3c7d985ee20
Julius <jzabulenas@protonmail.com>
2025-05-25 16:46:06 +0300

    Update Spring Boot to 3.5.0

--- backend/src/main/java/lt/techin/bookreservationapp/book/BookResponseDTO.java
+++ backend/src/main/java/lt/techin/bookreservationapp/book/BookResponseDTO.java
@@ -1,5 +1,4 @@
 package lt.techin.bookreservationapp.book;
 
-// TODO: should it be Long, or maybe long
 record BookResponseDTO(long id, String title, long userId) {
 }
--- backend/src/main/java/lt/techin/bookreservationapp/user/UserResponseDTO.java
+++ backend/src/main/java/lt/techin/bookreservationapp/user/UserResponseDTO.java
@@ -2,5 +2,6 @@ package lt.techin.bookreservationapp.user;
 
 import java.util.List;
 
+// Should this be primitive type long?
 public record UserResponseDTO(Long id, String email, List<Long> roles) {
 }
