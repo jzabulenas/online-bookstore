dc32a550dba1227b87670323e9131bd3244af70b
Julius <jzabulenas@protonmail.com>
2025-05-25 22:30:57 +0300

    Format code

--- backend/src/main/java/lt/techin/bookreservationapp/user/UserResponseDTO.java
+++ backend/src/main/java/lt/techin/bookreservationapp/user/UserResponseDTO.java
@@ -2,6 +2,7 @@ package lt.techin.bookreservationapp.user;
 
 import java.util.List;
 
-// Should this be primitive type long?
-public record UserResponseDTO(Long id, String email, List<Long> roles) {
+public record UserResponseDTO(Long id,
+    String email,
+    List<Long> roles) {
 }
