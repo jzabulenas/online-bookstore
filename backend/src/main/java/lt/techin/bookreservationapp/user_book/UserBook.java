58f703b02616b5f6d65bb0f42f047f5a80baf962
Julius <jzabulenas@protonmail.com>
2025-05-31 15:29:05 +0300

    Add UserBook class

---
+++ backend/src/main/java/lt/techin/bookreservationapp/user_book/UserBook.java
@@ -0,0 +1,58 @@
+package lt.techin.bookreservationapp.user_book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lt.techin.bookreservationapp.book.Book;
+import lt.techin.bookreservationapp.user.User;
+
+@Entity
+@Table(name = "users_books")
+public class UserBook {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @ManyToOne
+  private User user;
+
+  @NotNull
+  @ManyToOne
+  private Book book;
+
+  public UserBook(User user, Book book) {
+    this.user = user;
+    this.book = book;
+  }
+
+  public UserBook() {
+
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Book getBook() {
+    return book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+}
