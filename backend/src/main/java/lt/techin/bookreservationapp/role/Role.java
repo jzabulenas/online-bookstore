b084a6887654a184bf6fd9fc9b6f0028af2ee899
Julius <jzabulenas@protonmail.com>
2025-06-06 23:44:34 +0300

    Reorder getId of Role

--- backend/src/main/java/lt/techin/bookreservationapp/role/Role.java
+++ backend/src/main/java/lt/techin/bookreservationapp/role/Role.java
@@ -25,16 +25,16 @@ public class Role implements GrantedAuthority {
   public Role() {
   }
 
+  public Long getId() {
+    return id;
+  }
+
   public String getName() {
     return name;
   }
 
   public void setName(String name) {
     this.name = name;
-  }
-
-  public Long getId() {
-    return id;
   }
 
   @Override
