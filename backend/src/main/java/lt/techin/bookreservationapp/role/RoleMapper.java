a12ecdb95e8277e1012d9cafc95ce9ae4d501a1a
Julius <jzabulenas@protonmail.com>
2025-08-12 14:45:03 +0300

    Refactor saveUser method to use mapper classes

---
+++ backend/src/main/java/lt/techin/bookreservationapp/role/RoleMapper.java
@@ -0,0 +1,21 @@
+package lt.techin.bookreservationapp.role;
+
+import java.util.List;
+
+import lt.techin.bookreservationapp.user.User;
+
+public class RoleMapper {
+
+  public static List<Role> toEntities(List<Long> roleIds, RoleRepository roleRepository) {
+    return roleIds.stream()
+        .map(r -> roleRepository.findById(r).orElseThrow())
+        .toList();
+  }
+
+  public static List<Long> toIds(User user) {
+    return user.getRoles()
+        .stream()
+        .map(r -> r.getId())
+        .toList();
+  }
+}
--- backend/src/main/java/lt/techin/bookreservationapp/user/UserMapper.java
+++ backend/src/main/java/lt/techin/bookreservationapp/user/UserMapper.java
@@ -1,4 +1,26 @@
 package lt.techin.bookreservationapp.user;
 
-class UserMapper {
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lt.techin.bookreservationapp.role.Role;
+
+public class UserMapper {
+
+  public static User toEntity(UserRequestDTO userRequestDTO, PasswordEncoder passwordEncoder,
+      List<Role> roles) {
+    return new User(
+        userRequestDTO.email(),
+        passwordEncoder.encode(userRequestDTO.password()),
+        roles,
+        null);
+  }
+
+  public static UserResponseDTO toDTO(User user, List<Long> toRolesIds) {
+    return new UserResponseDTO(
+        user.getId(),
+        user.getEmail(),
+        toRolesIds);
+  }
 }
