9a955fbd2361e84973e92fb2f61cbc31e144ffb2
Julius <jzabulenas@protonmail.com>
2025-08-19 22:41:14 +0300

    Add desktop test "should click like on generated books, and not see them displayed in 'saved books' for other user"

--- frontend/tests/like-books-desktop.spec.ts
+++ frontend/tests/like-books-desktop.spec.ts
@@ -160,3 +160,88 @@ test("should click like on three generated books, and see them displayed in 'sav
       - paragraph
     `);
 });
+
+test("should click like on generated books, and not see them displayed in 'saved books' for other user", async ({
+  page,
+}) => {
+  const email = `antanas+${uuidv4()}@inbox.lt`;
+  const email2 = `antanas+${uuidv4()}@inbox.lt`;
+
+  // Sign up
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).fill(email);
+  await page.getByRole("textbox", { name: "Password:", exact: true }).click();
+  await page
+    .getByRole("textbox", { name: "Password:", exact: true })
+    .fill("12345678");
+  await page.getByRole("textbox", { name: "Confirm password:" }).click();
+  await page
+    .getByRole("textbox", { name: "Confirm password:" })
+    .fill("12345678");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.locator(".alert.alert-success.alert-dismissible").waitFor(); // Waits for success sign up message
+
+  // Log in
+  await page.getByRole("link", { name: "Log in" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).fill(email);
+  await page.getByRole("textbox", { name: "Password:" }).click();
+  await page.getByRole("textbox", { name: "Password:" }).fill("12345678");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  // Generate books, click "like" on them, retrieve text
+  await page.getByRole("textbox", { name: "Input your book:" }).click();
+  await page
+    .getByRole("textbox", { name: "Input your book:" })
+    .fill("The Merry Adventures of Robin Hood by Howard Pyle");
+  await page.getByRole("button", { name: "Submit" }).click();
+  const books = page.getByTestId("generated-books").locator("p");
+  await books.nth(0).getByRole("img").click();
+  await books.nth(1).getByRole("img").click();
+  await books.nth(2).getByRole("img").click();
+  const bookTitle1 = await books.nth(0).innerText();
+  const bookTitle2 = await books.nth(1).innerText();
+  const bookTitle3 = await books.nth(2).innerText();
+
+  // Log out
+  await page.getByRole("link", { name: "Log out" }).click();
+
+  // Sign up again
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).fill(email2);
+  await page.getByRole("textbox", { name: "Password:", exact: true }).click();
+  await page
+    .getByRole("textbox", { name: "Password:", exact: true })
+    .fill("12345678");
+  await page.getByRole("textbox", { name: "Confirm password:" }).click();
+  await page
+    .getByRole("textbox", { name: "Confirm password:" })
+    .fill("12345678");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.locator(".alert.alert-success.alert-dismissible").waitFor(); // Waits for success sign up message
+
+  // Log in
+  await page.getByRole("link", { name: "Log in" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).fill(email2);
+  await page.getByRole("textbox", { name: "Password:" }).click();
+  await page.getByRole("textbox", { name: "Password:" }).fill("12345678");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  // Go to "Saved books"
+  await page.getByRole("textbox", { name: "Input your book:" }).waitFor(); // Waits for the text seen after log in
+  await page.getByRole("link", { name: "Saved books" }).click();
+
+  // Assert
+  await expect(page).toHaveURL("http://localhost:5173/saved-books");
+  await expect(page.getByText("Books you have saved")).toBeVisible();
+  await expect(page.getByRole("heading")).toContainText("Books you have saved");
+  await expect(page.getByText(bookTitle1)).not.toBeVisible();
+  await expect(page.getByText(bookTitle2)).not.toBeVisible();
+  await expect(page.getByText(bookTitle3)).not.toBeVisible();
+  await expect(page).toHaveScreenshot();
+});
