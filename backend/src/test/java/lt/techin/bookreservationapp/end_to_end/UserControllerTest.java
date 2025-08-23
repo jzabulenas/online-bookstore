b0ca03e4f46ab528172eacce2baa0ab28d9f5f0e
Julius <jzabulenas@protonmail.com>
2025-08-23 19:54:45 +0300

    Add test `signup_whenEmailLocalPartIsTooLong_thenReturn400AndBody`

--- backend/src/test/java/lt/techin/bookreservationapp/end_to_end/UserControllerTest.java
+++ backend/src/test/java/lt/techin/bookreservationapp/end_to_end/UserControllerTest.java
@@ -140,6 +140,25 @@ class UserControllerTest {
           .body("$", aMapWithSize(1))
           .body("email", equalTo("Email must be at least 7 characters long"));
     }
+
+    @Test
+    void signup_whenEmailLocalPartIsTooLong_thenReturn400AndBody() throws JsonProcessingException {
+      String csrfToken = getCsrfToken();
+
+      given()
+          .cookie("XSRF-TOKEN", csrfToken)
+          .header("X-XSRF-TOKEN", csrfToken)
+          .contentType(ContentType.JSON)
+          .body(new ObjectMapper()
+              .writeValueAsString(new UserRequestDTO(
+                  "ivctsadyhqcfxzjinykxemzadbyajutuqzawknkckrgbzcjlwgufbrcycrdicezrv@gmail.com",
+                  "12345678", List.of(1L))))
+          .when()
+          .post("/signup")
+          .then()
+          .statusCode(400)
+          .body("$", aMapWithSize(1))
+          .body("email", equalTo("must be a well-formed email address"));
     }
 
     // TODO: should I test combinations? That is, for example, email and password is
