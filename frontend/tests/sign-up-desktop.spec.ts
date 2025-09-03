994408be21828e93bd4500cf335caf32818369f6
Julius <jzabulenas@protonmail.com>
2025-09-03 20:32:03 +0300

    Add test "should display an error message when password is of right length but does not contain lowercase letter"

--- frontend/tests/sign-up-desktop.spec.ts
+++ frontend/tests/sign-up-desktop.spec.ts
@@ -246,6 +246,34 @@ test("should display an error message when password is of right length but does 
   await expect(page).toHaveScreenshot();
 });
 
+test("should display an error message when password is of right length but does not contain lowercase letter", async ({
+  page,
+}) => {
+  const email = `antanas@inbox.lt`;
+
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Sign up" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).click();
+  await page.getByRole("textbox", { name: "Email:" }).fill(email);
+  await page.getByRole("textbox", { name: "Password:", exact: true }).click();
+  await page
+    .getByRole("textbox", { name: "Password:", exact: true })
+    .fill("S3FHCD4XMN!8T3");
+  await page.getByRole("textbox", { name: "Confirm password:" }).click();
+  await page
+    .getByRole("textbox", { name: "Confirm password:" })
+    .fill("S3FHCD4XMN!8T3");
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await expect(page).toHaveURL("http://localhost:5173/signup");
+  await expect(
+    page.getByText(
+      "Password must contain at least one uppercase and lowercase letter, number and any of these symbols: !@#$%^&*"
+    )
+  ).toBeVisible();
+  await expect(page).toHaveScreenshot();
+});
+
 // Confirm password
 //
 //
