c08a0b60b4689d2768c8d3ab7767e32ce9c4d569
Julius <jzabulenas@protonmail.com>
2025-09-13 16:43:10 +0300

    Add TODOs

--- backend/src/main/java/lt/techin/bookreservationapp/BookReservationAppApplication.java
+++ backend/src/main/java/lt/techin/bookreservationapp/BookReservationAppApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+// TODO: should log in in frontend fields have any validation. Also, the login frotnend fail test pass is 12345
+// TODO: sign up frotend tests, for example, password is too short is 12345. Obviously, have to add same validation as in backend. Confirm password test is also sussy
+// TODO: should frontend email valdation on signup be similar to backend? Maybe use server valdiation
 @SpringBootApplication
 class BookReservationAppApplication {
 
