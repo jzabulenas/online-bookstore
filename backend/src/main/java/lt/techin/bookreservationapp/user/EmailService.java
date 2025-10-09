34c1f443f5a1a39f40a2fa1d1370c9bcf9c9a636
Julius <jzabulenas@protonmail.com>
2025-10-09 19:49:56 +0300

    Add EmailService

---
+++ backend/src/main/java/lt/techin/bookreservationapp/user/EmailService.java
@@ -0,0 +1,81 @@
+package lt.techin.bookreservationapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+  private final JavaMailSender mailSender;
+  private final SpringTemplateEngine templateEngine;
+
+  @Autowired
+  public EmailService(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
+    this.mailSender = mailSender;
+    this.templateEngine = templateEngine;
+  }
+
+  /**
+   * Constructs the HTML content of the email message.
+   * 
+   * @param entity User who's verification code will be used in the URL for
+   *               verification.
+   * @return String containing the HTML code.
+   */
+  private String getVerificationMailContent(User entity) {
+    Context context = new Context();
+
+    // TODO: later will probably need to change to environment variable
+    String verificationUrl = String
+        .format("http://localhost:8080/verify?code=%s", entity.getVerificationCode());
+
+    // Injects the URL to the `applicationUrl` variable inside `user-verify.html`
+    // file
+    context.setVariable("applicationUrl", verificationUrl);
+
+    return templateEngine.process("user-verify", context);
+  }
+
+  /**
+   * Creates an email message deliverable, containing text, subject, from and to.
+   * 
+   * @param entity  User to who's email the message will be addressed.
+   * @param content content containing HTML message.
+   * @return MimeMessage
+   * @throws MessagingException
+   */
+  private MimeMessage createMessage(User entity, String content) throws MessagingException {
+    MimeMessage mimeMessage = mailSender.createMimeMessage();
+    MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+    message.setText(content, true);
+    message.setSubject("Welcome to MyApp XYZ");
+    message.setFrom("noreply@myapp.xyz");
+    message.setTo(entity.getEmail());
+
+    return mimeMessage;
+  }
+
+  /**
+   * Sends verification email to a user.
+   * 
+   * @param entity the User to whom a verification email will be sent to.
+   */
+  public void sendVerificationMail(User entity) throws UserMailFailedException {
+    String content = getVerificationMailContent(entity);
+
+    try {
+      mailSender.send(createMessage(entity, content));
+    } catch (MessagingException ex) {
+      throw new UserMailFailedException(
+          "Could not send e-mail to verify user with e-mail '" + entity.getEmail() + "'", ex);
+    }
+  }
+
+}
