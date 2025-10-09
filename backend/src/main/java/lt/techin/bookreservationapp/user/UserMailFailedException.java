2cb3791f2a43a39c68c9257ba5074120486dbdce
Julius <jzabulenas@protonmail.com>
2025-10-09 19:55:42 +0300

    Add `UserMailFailedException`

---
+++ backend/src/main/java/lt/techin/bookreservationapp/user/UserMailFailedException.java
@@ -0,0 +1,9 @@
+package lt.techin.bookreservationapp.user;
+
+public class UserMailFailedException extends Exception {
+
+  public UserMailFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
