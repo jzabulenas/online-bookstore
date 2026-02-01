62a6e0772b83fed6070121b19fde1d67b322b510
Julius <jzabulenas@protonmail.com>
2026-02-01 17:52:49 +0200

    Update `whenUserSignsUp_thenReturn201AndBody` test
    Extract `id` from response, and use it to verify `Location` header value.

--- backend/src/test/java/lt/techin/bookreservationapp/end_to_end/SignUpTest.java
+++ backend/src/test/java/lt/techin/bookreservationapp/end_to_end/SignUpTest.java
@@ -1,15 +1,14 @@
 package lt.techin.bookreservationapp.end_to_end;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.equalTo;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SignUpTest {
@@ -20,32 +19,37 @@ class SignUpTest {
     UUID uuid = UUID.randomUUID();
     String email = "antanas" + uuid + "@gmail.com";
 
-    // Sign up
-    given()
-        .cookie("XSRF-TOKEN", csrfToken)
-        .header("X-XSRF-TOKEN", csrfToken)
-        .contentType(ContentType.JSON)
-        .body(
-            """
-              {
-                "email": "%s",
-                "password": "r9$CbHEaGXLUsP",
-                "roles": [
-                   1
-                ]
-              }
-            """
-                .formatted(email))
-        .when()
-        .post("/signup")
-        .then()
-        .statusCode(201)
-        .body("$", aMapWithSize(3))
-        .body("id", greaterThan(0))
-        .body("email", equalTo(email))
-        .body("roles", hasSize(1))
-        .body("roles[0]", equalTo(1))
-        .header("Location", containsString("http://localhost:8080/signup/"));
+    // Send sign up request
+    Response response =
+        given()
+            .cookie("XSRF-TOKEN", csrfToken)
+            .header("X-XSRF-TOKEN", csrfToken)
+            .contentType(ContentType.JSON)
+            .body(
+                """
+                {
+                  "email": "%s",
+                  "password": "r9$CbHEaGXLUsP",
+                  "roles": [
+                     1
+                  ]
+                }
+                """
+                    .formatted(email))
+            .when()
+            .post("/signup")
+            .then()
+            .statusCode(201)
+            .body("$", aMapWithSize(3))
+            .body("email", equalTo(email))
+            .body("roles", hasSize(1))
+            .body("roles[0]", equalTo(1))
+            .extract()
+            .response();
+
+    int id = response.path("id");
+    assertThat(id, greaterThan(0));
+    assertThat(response.getHeader("Location"), equalTo("http://localhost:8080/signup/" + id));
 
     // Extract verification link from message
     String verificationSnippet =
@@ -62,7 +66,7 @@ class SignUpTest {
     Pattern pattern = Pattern.compile("(http://localhost:8080/verify\\?code=[^\\s]+)");
     Matcher matcher = pattern.matcher(verificationSnippet);
 
-    Assertions.assertTrue(matcher.find(), "Verification link not found in email snippet");
+    assertThat(matcher.find(), equalTo(true));
     String verificationLink = matcher.group(1);
 
     // Send verification request to extracted link
