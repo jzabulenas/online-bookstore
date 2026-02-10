8d47ac1542bf60030368ca5d9204dd2e1ac480f2
Julius <jzabulenas@protonmail.com>
2026-02-10 21:04:07 +0200

    Add test `whenUserHasBooksTheyLikedBefore_thenNotSeeThemInNewlyGeneratedBooks`

--- backend/src/test/java/lt/techin/bookreservationapp/end_to_end/GenerateBooksTest.java
+++ backend/src/test/java/lt/techin/bookreservationapp/end_to_end/GenerateBooksTest.java
@@ -1,24 +1,22 @@
 package lt.techin.bookreservationapp.end_to_end;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.aMapWithSize;
-import static org.hamcrest.Matchers.hasSize;
-
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.junit.jupiter.api.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
 
 class GenerateBooksTest {
 
   @Test
-  void whenBooksAreGenerated_thenReturn200AndListOfBooks() throws JsonProcessingException {
+  void whenBooksAreGenerated_thenReturn200AndListOfBooks() {
     String csrfToken = this.getCsrfToken();
     Response logInResponse = createUserThenLogInAndGetSession();
 
@@ -32,13 +30,125 @@ class GenerateBooksTest {
     		{
     		  "message": "Dracula by Bram Stoker"
     		}
-            """)
+    		""")
         .when()
-        .post("/generate-books")
+        .post("http://localhost:8080/generate-books")
         .then()
         .statusCode(200)
         .body("$", aMapWithSize(1))
         .body("result", hasSize(3));
+  }
+
+  @Test
+  void whenUserHasBooksTheyLikedBefore_thenNotSeeThemInNewlyGeneratedBooks() {
+    String csrfToken = this.getCsrfToken();
+    Response logInResponse = createUserThenLogInAndGetSession();
+
+    // Generate new books
+    Response newlyGeneratedBooksResponse =
+        given()
+            .cookie("JSESSIONID", logInResponse.getSessionId())
+            .cookie("XSRF-TOKEN", csrfToken)
+            .header("X-XSRF-TOKEN", csrfToken)
+            .contentType(ContentType.JSON)
+            .body(
+                """
+				{
+				  "message": "Dracula by Bram Stoker"
+				}
+				""")
+            .when()
+            .post("http://localhost:8080/generate-books")
+            .then()
+            .statusCode(200)
+            .body("$", aMapWithSize(1))
+            .body("result", hasSize(3))
+            .extract()
+            .response();
+    List<String> newlyGeneratedBooksList = newlyGeneratedBooksResponse.jsonPath().getList("result");
+
+    // Like generated books for user
+    given()
+        .cookie("JSESSIONID", logInResponse.getSessionId())
+        .cookie("XSRF-TOKEN", csrfToken)
+        .header("X-XSRF-TOKEN", csrfToken)
+        .contentType(ContentType.JSON)
+        .body(
+            """
+			{
+			  "title": "%s"
+			}
+			"""
+                .formatted(newlyGeneratedBooksList.get(0)))
+        .when()
+        .post("http://localhost:8080/books");
+
+    given()
+        .cookie("JSESSIONID", logInResponse.getSessionId())
+        .cookie("XSRF-TOKEN", csrfToken)
+        .header("X-XSRF-TOKEN", csrfToken)
+        .contentType(ContentType.JSON)
+        .body(
+            """
+			{
+			  "title": "%s"
+			}
+			"""
+                .formatted(newlyGeneratedBooksList.get(1)))
+        .when()
+        .post("http://localhost:8080/books");
+
+    given()
+        .cookie("JSESSIONID", logInResponse.getSessionId())
+        .cookie("XSRF-TOKEN", csrfToken)
+        .header("X-XSRF-TOKEN", csrfToken)
+        .contentType(ContentType.JSON)
+        .body(
+            """
+			{
+			  "title": "%s"
+			}
+			"""
+                .formatted(newlyGeneratedBooksList.get(2)))
+        .when()
+        .post("http://localhost:8080/books");
+
+    // Generate new books, second time
+    Response newlyGeneratedBooksResponse2 =
+        given()
+            .cookie("JSESSIONID", logInResponse.getSessionId())
+            .cookie("XSRF-TOKEN", csrfToken)
+            .header("X-XSRF-TOKEN", csrfToken)
+            .contentType(ContentType.JSON)
+            .body(
+                """
+                  {
+                    "message": "Dracula by Bram Stoker"
+                  }
+                """)
+            .when()
+            .post("http://localhost:8080/generate-books")
+            .then()
+            .statusCode(200)
+            .body("$", aMapWithSize(1))
+            .body("result", hasSize(3))
+            .extract()
+            .response();
+    List<String> newlyGeneratedBooksList2 =
+        newlyGeneratedBooksResponse2.jsonPath().getList("result");
+
+    // Retrieve liked books of user
+    Response existingUserBooksResponse =
+        given()
+            .cookie("JSESSIONID", logInResponse.getSessionId())
+            .cookie("XSRF-TOKEN", csrfToken)
+            .header("X-XSRF-TOKEN", csrfToken)
+            .when()
+            .get("/books");
+    List<String> existingUserBooksList = existingUserBooksResponse.jsonPath().getList("title");
+
+    assertThat(
+        Collections.disjoint(newlyGeneratedBooksList2, existingUserBooksList), equalTo(true));
   }
 
   private Response createUserThenLogInAndGetSession() {
@@ -62,10 +172,10 @@ class GenerateBooksTest {
                  1
               ]
             }
-	        """
+            """
                 .formatted(emailForSigningUp))
         .when()
-        .post("/signup");
+        .post("http://localhost:8080/signup");
 
     // Extract verification link from email message
     String verificationSnippet =
@@ -94,7 +204,7 @@ class GenerateBooksTest {
         .header("X-XSRF-TOKEN", csrfToken)
         .contentType(ContentType.URLENC)
         .body("username=%s&password=r9$CbHEaGXLUsP".formatted(emailForLoggingIn))
-        .post("/login")
+        .post("http://localhost:8080/login")
         .then()
         .extract()
         .response();
